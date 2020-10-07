d29eb33b06bf16fcbb5c62a083b55a24a4f04ab7
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2020-10-07 10:11:09 -0400

    Testers and coders are now an option to sort.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ Coder Tester lab
 
 //Status update for 6 October 2020
 /*
-The pairing method now works! Just to aplhabitize it and i think i need to give the user
-the option to sort it a specific way, not too sure.
+Coder sorting is complete, just got to do testing and then let user pick choice
  */
 import java.io.*;
 import java.lang.Math;
@@ -27,11 +26,17 @@ public class Main {
         System.out.println(Arrays.toString(usedCoder));
         // intiizligin variables and creating array list
         File file;
+        //String TempeoryStorage is for coders first
         String tempeoryStorage = null;
+        //String testersTemperory is for testers first
+        String testersTempeory = null;
         //String [] studentPairs = new String[33];
-        List <String> studentPairs = new ArrayList<>();
+        List <String> studentPairsCodersFirst = new ArrayList<>();
+        List <String> studentPairsTestersFirst = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         List<String> allStudents = new ArrayList<>();
+
+
 
         //String[] studentsAll = allStudents.toArray(new String[33]);
 
@@ -40,6 +45,7 @@ public class Main {
 
 
             //Reading file into an array using bufferreader
+
         try {
             file = new File("C:\\Users\\mpari\\Documents\\coding projects\\Java\\Coder Tester Software Design Program\\src\\SD_ClassList.txt");
             br = new BufferedReader(new FileReader(String.valueOf(file)));
@@ -49,24 +55,72 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+
         //Asking user for how many students do they want
         System.out.println("How many students should be in pairs?");
             numOfStudents = in.nextInt();
+
+        System.out.println("Do you want your list to be sorted coders first or testers first? Note: Choose" +
+                " Testers first if you want it sorted by testers. And vice versa.");
+
+        String codersOrTesters = in.next();
+
+        //This if statement is coders first
+        if (codersOrTesters.toLowerCase().equals("coders")) {
+
             int i = 0;
             while (i != numOfStudents) {
                 i++;
 
-                // method
-                tempeoryStorage = ((studentsAll(allStudents, numOfStudents)));
-                studentPairs.add(tempeoryStorage);
+                // method for coders first
+                tempeoryStorage = ((studentsAllCoders(allStudents, numOfStudents)));
+                studentPairsCodersFirst.add(tempeoryStorage);
             }
-            String temp;
-        studentPairs.sort( Comparator.comparing( String::toString ) );
 
-        System.out.println(studentPairs);
+        studentPairsCodersFirst.sort( Comparator.comparing( String::toString ) );
+        System.out.println(studentPairsCodersFirst);
     }
 
-    public static String studentsAll(List<String> allStudents, int numOfStudents) {
+        else {
+            int i = 0;
+            while (i != numOfStudents) {
+                i++;
+
+                // method for coders first
+                testersTempeory= ((studentsAllTesters(allStudents, numOfStudents)));
+                studentPairsTestersFirst.add(testersTempeory);
+            }
+
+            studentPairsTestersFirst.sort( Comparator.comparing( String::toString ) );
+            System.out.println(studentPairsTestersFirst);
+
+        }
+    }
+
+    private static String studentsAllTesters(List<String> allStudents, int numOfStudents) {
+       while (true) {
+           //common variable for randomindex
+           int studentsAllLength = allStudents.size();
+           //random index and inputting arraylist value into a string
+           int randomIndex = (int) (Math.random() * studentsAllLength);
+           // String randomElement = allStudents.get(randomIndex); Trying to see if using array directly instead of array will work better
+           int randomIndex2 = (int) (Math.random() * studentsAllLength);
+           //String randomElement2 = allStudents.get(randomIndex2);
+           if ((!usedTester[randomIndex2] && !usedCoder[randomIndex]) && randomIndex != randomIndex2) {
+               //  System.out.println(randomIndex);
+               // System.out.println(randomIndex2);
+               // System.out.println(Arrays.toString(usedCoder));
+               // System.out.println(Arrays.toString(usedTester));
+               String last = allStudents.get(randomIndex2) + "," + allStudents.get(randomIndex);
+               usedCoder[randomIndex2] = true;
+               usedTester[randomIndex] = true;
+               return last;
+           }
+       }
+    }
+
+    public static String studentsAllCoders(List<String> allStudents, int numOfStudents) {
         while (true) {
             //common variable for randomindex
             int studentsAllLength = allStudents.size();
