c5b37e8c109066a4450446c240e0ed8e8b44bfca
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2020-10-18 13:50:23 -0400

    final revision

--- src/Main.java
+++ src/Main.java
@@ -27,24 +27,25 @@ public class Main {
         //printing out to make sure the values are starting out as false
         // initialize variables and creating array list
         File file;
-        //String TempeoryStorage is for coders first
-        String tempeoryStorage = null;
-        //String testersTemperory is for testers first
-        String testersTempeory = null;
 
-        List <String> studentPairsCodersFirst = new ArrayList<>();
-        List <String> studentPairsTestersFirst = new ArrayList<>();
+        String tempeoryStorage = null;
+
+
+        List<String> studentPairsCodersFirst = new ArrayList<>();
+
+        // List<String> studentPairsTestersFirst = new ArrayList<>();
+
+        List<String> testersGroupInCodersFirstRun = new ArrayList<>();
+
         Scanner in = new Scanner(System.in);
         List<String> allStudents = new ArrayList<>();
-
-
 
 
         BufferedReader br;
         int numOfStudents;
 
 
-            //Reading file into an array using bufferreader
+        //Reading file into an array using bufferreader
 
 
         try {
@@ -56,7 +57,6 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
 
 
         //Asking user for how many students do they want
@@ -73,17 +73,7 @@ public class Main {
         }
 
 
-        System.out.println("Do you want your list to be sorted coders first or testers first? Note: Choose" +
-                " Testers first if you want it sorted by testers. And vice versa. Also, for example," +
-                " if you pick coders first, they will print then testers first will print with the same" +
-                " names.");
 
-
-        String codersOrTesters = in.next();
-
-
-        //This if statement is coders first
-        if (codersOrTesters.toLowerCase().equals("coders")) {
 
             int i = 0;
 
@@ -94,6 +84,7 @@ public class Main {
                 // method for coders first
                 tempeoryStorage = ((studentsAllCoders(allStudents, numOfStudents)));
                 studentPairsCodersFirst.add(tempeoryStorage);
+                testersGroupInCodersFirstRun.add(tempeoryStorage);
             }
 
 
@@ -106,31 +97,64 @@ public class Main {
             System.out.println("-----------------------------------------------");
 
 
-            //for loop to run for amount of coders and then also to split the string and output
-            for (int P = 0; P < studentPairsCodersFirst.size(); P++) {
-                String value = studentPairsCodersFirst.get(P);
-                String[] split = value.split(",");
-                String names = String.format("%10S %19S ", split[0], split[1]);
-                System.out.println(names);
+        //for loop to run for amount of coders and then also to split the string and output
+        for (int P = 0; P < studentPairsCodersFirst.size(); P++) {
+            String value = studentPairsCodersFirst.get(P);
+            String[] split = value.split(",");
+            String names = String.format("%10S %19S ", split[0], split[1]);
+            System.out.println(names);
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        // oraganizing the array into testers first
+        for (int w = 0; w < testersGroupInCodersFirstRun.size(); w++) {
+            String value = testersGroupInCodersFirstRun.get(w);
+            String[] split = value.split(",");
+            String names = String.format("%10S %19S", split[1], split[0]);
+        }
+        //sorting function
+        testersGroupInCodersFirstRun.sort(Comparator.comparing(String::toString));
+
+
+        //final output
+        firstLine = String.format("%10S %19S ", " Testers", "Coders");
+        System.out.println(firstLine);
+        secondLine = String.format("%10S %19S ", " Last Name", " Last Name");
+        System.out.println(secondLine);
+        System.out.println("-----------------------------------------------");
+
+
+        for (int C = 0; C < testersGroupInCodersFirstRun.size(); C++) {
+            String value = testersGroupInCodersFirstRun.get(C);
+            String[] split = value.split(",");
+            String names = String.format("%10S %19S ", split[0], split[1]);
+            System.out.println(names);
+        }
+    }
+
+    public static String studentsAllCoders(List<String> allStudents, int numOfStudents) {
+        while (true) {
+            //common variable for randomindex
+            int studentsAllLength = allStudents.size();
+            //random index and inputting arraylist value into a string
+            int randomIndex = (int) (Math.random() * studentsAllLength);
+            int randomIndex2 = (int) (Math.random() * studentsAllLength);
+            if ((!usedTester[randomIndex] && !usedCoder[randomIndex2]) && randomIndex != randomIndex2) {
+                String last = allStudents.get(randomIndex2) + "," + allStudents.get(randomIndex);
+                usedCoder[randomIndex2] = true;
+                usedTester[randomIndex] = true;
+                return last;
             }
+        }
+    }
+}
 
 
-            System.out.println(" ");
-            System.out.println(" ");
-            firstLine = String.format("%10S %19S ", " Testers", "Coders");
-            System.out.println(firstLine);
-            secondLine = String.format("%10S %19S ", " Last Name", " Last Name");
-            System.out.println(secondLine);
-            System.out.println("-----------------------------------------------");
 
 
-            for (int P = 0; P < studentPairsCodersFirst.size(); P++) {
-                String value = studentPairsCodersFirst.get(P);
-                String[] split = value.split(",");
-                String names = String.format("%10S %19S ", split[1], split[0]);
-                System.out.println(names);
-            }
-        } else {
+    /*
+        else {
             int i = 0;
             while (i != numOfStudents) {
                 i++;
@@ -191,20 +215,5 @@ public class Main {
     }
 
 
-    public static String studentsAllCoders(List<String> allStudents, int numOfStudents) {
-        while (true) {
-            //common variable for randomindex
-            int studentsAllLength = allStudents.size();
-            //random index and inputting arraylist value into a string
-            int randomIndex = (int) (Math.random() * studentsAllLength);
-            int randomIndex2 = (int) (Math.random() * studentsAllLength);
-            if ((!usedTester[randomIndex] && !usedCoder[randomIndex2]) && randomIndex != randomIndex2)
-            {
-                String last = allStudents.get(randomIndex2) + "," + allStudents.get(randomIndex);
-                usedCoder[randomIndex2] = true;
-                usedTester[randomIndex] = true;
-                return last;
-            }
-        }
-    }
-}
+
+         */
